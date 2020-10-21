7c59c0485a166c671c5c9e6c9e13d34d25551d20
marcus goldberger <goldbergerm7@gmail.com>
2020-10-20 19:09:41 -0700

    got character names, and worked on string manipulation for that

--- marvel/__init__.py
+++ marvel/__init__.py
@@ -9,15 +9,36 @@ series
 
 from marvel import api
 
+def get_characters_by_series(series):
+    return api.get("characters", params={"series": series})
+
 def get_character_by_name(name):
     return api.get("characters", params={"name": name})
 
-def character_name(character):
-    results = get_results(character)
+def character_name(character_resp):
+    # return character["data"]["results"][0]["name"]
+    results = get_results(character_resp)
     return results[0]["name"]
-
-def get_characters_by_series(series):
-    return api.get("characters", params={"series": series})
     
-def get_results(response_body):
-    return response_body["data"]["results"]
+def get_results(character_resp):
+    return character_resp["data"]["results"]
+
+def character_names(character_resp):
+    results = get_results(character_resp)
+    return [character["name"] for character in results]
+
+SAMPLE_NAMES = ['Daken',
+ 'Dark Avengers',
+ 'Hulk',
+ 'Sentry (Robert Reynolds)',
+ 'She-Hulk (Jennifer Walters)',
+ 'She-Hulk (Lyra)',
+ 'Thundra']
+
+def split_name(name):
+    raw_names = name.split(" ")
+    return [p.replace("(", "").replace(")", "") for p in raw_names]
+
+def split_names(list_of_names):
+    return [name for name in list_of_names]
+    
